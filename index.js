eb44e1ea11e8279d02d11dd50a9cffe2717d5703
Jesse Hull <jse.hull@gmail.com>
2020-04-24 17:28:30 -0600

    add basic turn val + player highlight + news feed

--- index.js
+++ index.js
@@ -1,79 +1,16 @@
+// TODO - dynamic player turn
+
+
 const floorTilesArr = [
-  {
-    color: 'red'
-  },
-  {
-    color: 'red'
-  },
-  {
-    color: 'red'
-  },
-  {
-    color: 'cyan'
-  },
-  {
-    color: 'cyan'
-  },
-  {
-    color: 'cyan'
-  },
-  {
-    color: 'green'
-  },
-  {
-    color: 'green'
-  },
-  {
-    color: 'green'
-  },
-  {
-    color: 'orange'
-  },
-  {
-    color: 'orange'
-  },
-  {
-    color: 'orange'
-  },
-  {
-    color: 'pink'
-  },
-  {
-    color: 'pink'
-  },
-  {
-    color: 'pink'
-  },
-  {
-    color: 'purple'
-  },
-  {
-    color: 'purple'
-  },
-  {
-    color: 'purple'
-  },
-  {
-    color: 'yellow'
-  },
-  {
-    color: 'yellow'
-  },
-  {
-    color: 'yellow'
-  },
-  {
-    color: 'blue'
-  },
-  {
-    color: 'blue'
-  },
-  {
-    color: 'blue'
-  },
-  {
-    color: 'wild'
-  }
+  { color: 'red' }, { color: 'red' }, { color: 'red' },
+  { color: 'cyan' }, { color: 'cyan' }, { color: 'cyan' },
+  { color: 'green' }, { color: 'green' }, { color: 'green' },
+  { color: 'orange' }, { color: 'orange' }, { color: 'orange' },
+  { color: 'pink' }, { color: 'pink' }, { color: 'pink' },
+  { color: 'purple' }, { color: 'purple' }, { color: 'purple' },
+  { color: 'yellow' }, { color: 'yellow' }, { color: 'yellow' },
+  { color: 'blue' }, { color: 'blue' }, { color: 'blue' },
+  { color: 'wild' }
 ]
 
 const danceFloor = document.getElementById('dance-floor')
@@ -100,21 +37,55 @@ function lightUpTile() {
   const tileId = this.getAttribute('tile-id')
   tilesChosen.push(floorTilesArr[tileId].color)
   console.log(tilesChosen)
-  console.log(floorTilesArr[tileId].color)
 
   // floorTile.classList.add(floorTilesArr[i].color)
 
-  validateTiles()
+  getPlayerTurn()
 }
 
-function validateTiles() {
+// let player = document.getElementsByClassName('player')
+const playerOne = document.getElementById('player-one')
+const playerTwo = document.getElementById('player-two')
+
+
+// const playerAny = document.getElementsByClassName('player')
+// console.log('playerAny', playerAny)
+let newsFeed = document.getElementById('news-feed')
+
+function getPlayerTurn() {
   if (tilesChosen.length === 3) {
-    if (tilesChosen[0] === tilesChosen[1] && tilesChosen[1] === tilesChosen[2]) {
-      console.log('POINTS!')
-    } else if (tilesChosen.includes('wild')) {
-      console.log('Bonus try!')
-    } else {
-      console.log('sorry, end of turn')
+    validateTurn()
+  }
+
+  return
+}
+
+function updateNewsFeed(feedMessage) {
+    window.setTimeout(() => {
+      newsFeed.innerText = 'Player1 ' + feedMessage
+    }, 200)
+
+    // TODO - update feed on player turn change
+}
+
+function updateActivePlayer() {
+    if (playerOne.classList.contains('active')) {
+      playerOne.classList.remove('active')
+      playerTwo.classList.add('active')
+    } else if (playerTwo.classList.contains('active')) {
+      playerName.classList.remove('active')
+      playerOne.classList.add('active')
+      playerTwo.classList.remove('active')
     }
+}
+
+function validateTurn() {
+  if (tilesChosen[0] === tilesChosen[1] && tilesChosen[1] === tilesChosen[2]) {
+    updateNewsFeed('gets a point')
+  } else if (tilesChosen.includes('wild')) {
+    updateNewsFeed('gets a BONUS turn')
+  } else {
+    updateNewsFeed('turn over')
+    updateActivePlayer()
   }
 }
