541d0ed1422ee42231f4cdfe3eb87c4a7ab2e2b2
Jesse Hull <jse.hull@gmail.com>
2020-04-24 11:36:19 -0600

    add basic tile select validation

--- index.js
+++ index.js
@@ -85,9 +85,9 @@ loadDanceFloor()
 function loadDanceFloor() {
   for (let i = 0; i < floorTilesArr.length; i++) {
     floorTile = document.createElement('div')
-    // console.log('const', floorTile)
     floorTile.classList.add('floor-tile')
     floorTile.setAttribute('tile-id', i)
+
     floorTile.addEventListener('click', lightUpTile)
     // FIXME unable to access floorTilesArr[i].color
 
@@ -99,9 +99,22 @@ function loadDanceFloor() {
 function lightUpTile() {
   const tileId = this.getAttribute('tile-id')
   tilesChosen.push(floorTilesArr[tileId].color)
-  console.log('tile clicks')
   console.log(tilesChosen)
+  console.log(floorTilesArr[tileId].color)
 
-  // TODO light up tile on click
   // floorTile.classList.add(floorTilesArr[i].color)
+
+  validateTiles()
+}
+
+function validateTiles() {
+  if (tilesChosen.length === 3) {
+    if (tilesChosen[0] === tilesChosen[1] && tilesChosen[1] === tilesChosen[2]) {
+      console.log('POINTS!')
+    } else if (tilesChosen.includes('wild')) {
+      console.log('Bonus try!')
+    } else {
+      console.log('sorry, end of turn')
+    }
+  }
 }
