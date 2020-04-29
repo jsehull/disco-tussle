6ec99da16b3fbf7c16fce2fc1f2e086a30a2ac22
Jesse Hull <jse.hull@gmail.com>
2020-04-29 15:35:32 -0600

    add visual match pair + visual turn end

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// TODO - dynamic player turn
-
-
 const floorTilesArr = [
   { color: 'red' }, { color: 'red' }, { color: 'red' },
   { color: 'cyan' }, { color: 'cyan' }, { color: 'cyan' },
@@ -15,7 +12,6 @@ const floorTilesArr = [
 
 const danceFloor = document.getElementById('dance-floor')
 let floorTile
-let tilesChosen = []
 
 loadDanceFloor()
 
@@ -29,57 +25,90 @@ function loadDanceFloor() {
   }
 }
 
+let tileEl
+let tileId
+let tilesChosen = []
+let tileElsChosen = []
+
 function lightUpTile(e) {
-  const tileId = e.target.getAttribute('tile-id')
-  tilesChosen.push(floorTilesArr[tileId].color)
+  tileEl = e.target
+  tileId = e.target.getAttribute('tile-id')
+  tilesChosen.push(floorTilesArr[tileId])
+  tileElsChosen.push(tileEl)
   e.target.classList.add(floorTilesArr[tileId].color)
 
-  getPlayerTurn()
+  showPickCountInFeed()
+
+  if (tilesChosen.length === 3) {
+    validateTurn(tileElsChosen, tilesChosen)
+  }
 }
 
+function validateTurn(tileElsChosen, tilesChosen) {
+  const tileElOne = tileElsChosen[0]
+  const tileElTwo = tileElsChosen[1]
+  const tileElThree = tileElsChosen[2]
+  let tilesMatch = tilesChosen[0].color === tilesChosen[1].color
+    && tilesChosen[1].color === tilesChosen[2].color
+
+  console.log(tilesChosen)
+
+  if (tilesMatch) {
+    resetPicksInFeed('You get one point')
+    window.setTimeout(() => {
+      tileElOne.classList.add('matching')
+      tileElTwo.classList.add('matching')
+      tileElThree.classList.add('matching')
+    }, 250)
+  } else if (tilesChosen.includes('wild')) {
+    // FIXME no longer recognizes WILD
+    resetPicksInFeed('You get a BONUS turn')
+  } else {
+    resetPicksInFeed('Try again')
+    window.setTimeout(() => {
+      tileElOne.classList.remove(tilesChosen[0].color)
+      tileElTwo.classList.remove(tilesChosen[1].color)
+      tileElThree.classList.remove(tilesChosen[2].color)
+    }, 800)
+    // changeActivePlayer() // activate for multiplayer
+  }
+}
+
+let newsFeed = document.getElementById('news-feed')
+
+function showPickCountInFeed() {
+  if (tilesChosen.length === 1) {
+    newsFeed.innerText = '2 picks remaining'
+  } else if (tilesChosen.length === 2) {
+    newsFeed.innerText = '1 picks remaining'
+  }
+}
+
+function resetPicksInFeed(feedMessage) {
+    newsFeed.innerText = feedMessage
+
+    window.setTimeout(() => {
+      newsFeed.innerText = '3 picks remaining'
+    }, 800)
+}
+
+
+
 // let player = document.getElementsByClassName('player')
-const playerOne = document.getElementById('player-one')
-const playerTwo = document.getElementById('player-two')
+// const playerOne = document.getElementById('player-one')
+// const playerTwo = document.getElementById('player-two')
 
 
 // const playerAny = document.getElementsByClassName('player')
 // console.log('playerAny', playerAny)
-let newsFeed = document.getElementById('news-feed')
 
-function getPlayerTurn() {
-  if (tilesChosen.length === 3) {
-    validateTurn()
-  }
-
-  return
-}
-
-function updateNewsFeed(feedMessage) {
-    window.setTimeout(() => {
-      newsFeed.innerText = 'Player1 ' + feedMessage
-    }, 200)
-
-    // TODO - update feed on player turn change
-}
-
-function changeActivePlayer() {
-    if (playerOne.classList.contains('active')) {
-      playerOne.classList.remove('active')
-      playerTwo.classList.add('active')
-    } else if (playerTwo.classList.contains('active')) {
-      playerName.classList.remove('active')
-      playerOne.classList.add('active')
-      playerTwo.classList.remove('active')
-    }
-}
-
-function validateTurn() {
-  if (tilesChosen[0] === tilesChosen[1] && tilesChosen[1] === tilesChosen[2]) {
-    updateNewsFeed('gets a point')
-  } else if (tilesChosen.includes('wild')) {
-    updateNewsFeed('gets a BONUS turn')
-  } else {
-    updateNewsFeed('turn over')
-    changeActivePlayer()
-  }
-}
+// function changeActivePlayer() { // activate for multiplayer
+//     if (playerOne.classList.contains('active')) {
+//       playerOne.classList.remove('active')
+//       playerTwo.classList.add('active')
+//     } else if (playerTwo.classList.contains('active')) {
+//       playerName.classList.remove('active')
+//       playerOne.classList.add('active')
+//       playerTwo.classList.remove('active')
+//     }
+// }
